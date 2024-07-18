dcf77255ffaf0cb05e36f279d1b1fea8863cf3d9
thaibaohaonhien1995@gmail.com <Mazerunnerone2>
2024-07-19 05:08:09 +0700

    BAI18_LOADER_CSS_SCSS

--- BAI16_NODEJS_NPM/src/index.js
+++ BAI16_NODEJS_NPM/src/index.js
@@ -1,4 +1,6 @@
 import sum from './utils'
 
+import './styles/styles.css'
+import './styles/styles.scss'
 console.log(sum(100,40))
 
--- BAI16_NODEJS_NPM/webpack.config.js
+++ BAI16_NODEJS_NPM/webpack.config.js
@@ -1,17 +1,32 @@
 const path = require('path') // lấy thằng path từ nodejs
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 console.log('__dirname',__dirname);
 console.log('path.resolve', path.resolve());
 console.log(`path.resolve(__dirname, 'dist')`, path.resolve(__dirname, 'dist'));
 
 module.exports = {
-    mode: 'development', 
+    mode: 'production', 
     entry: {
         app: path.resolve('src/index.js')
     },
     output: {
         path: path.resolve(__dirname, 'dist')
         // filename: 'script.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss|css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugin: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html'
+        })
+    ]
 };
 
